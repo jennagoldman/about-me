fb24872e9b73e16b76e393fc15e0c32960a97a2e
Jenna Goldman <jenna.goldman89@gmail.com>
2020-01-14 20:43:13 -0800

    finish styling page and write function test

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,41 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
-
+import compareNumbers from './compareNumbers.js';
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+test('should be equal, resulting in 0', function(assert) {
     //Arrange
     // Set up your parameters and expectations
 
     //Act 
     // Call the function you're testing and set the result to a const
-
+    const shouldBeEqual = compareNumbers(7, 7);
     //Assert
     // Make assertions about what is expected valid result
-    assert.equal(true, false);
+    assert.equal(shouldBeEqual, 0);
 });
+
+test('should be less than, resulting in -1', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+
+    // ACT - Call the function you're testing and set the result to a const
+    const shouldBeLessThan = compareNumbers(5, 7);
+
+    // ASSERT - Make assertions about what is expected valid result
+    assert.equal(shouldBeLessThan, -1);
+});
+
+test('should be greater than, resulting in 1', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+
+    // ACT - Call the function you're testing and set the result to a const
+    const shouldBeGreaterThan = compareNumbers(8, 7);
+
+    // ASSERT - Make assertions about what is expected valid result
+    assert.equal(shouldBeGreaterThan, 1);
+});
+
+// STRETCH GOAL
+// Actually test for invalid input. You should throw an error if either parameter is not a number. Consult the QUnit docs for how to test for an exception.
